63bfc22938dce827f3c876530702c862dcb4c280
Gerijek O <makopiyo45@gmail.com>
2024-07-14 10:58:28 +0300

    Changed the name of the company from MSN to BBC

--- src/Emailapp/Email.java
+++ src/Emailapp/Email.java
@@ -13,7 +13,7 @@ public class Email {
   private String alternateEmail;
   private int passwordLength = 10;
   private String email;
-  private String companySuffix = "msncomopany.com";
+  private String companySuffix = "BBCcomopany.com";
 
   // constructor to initialize names
 public Email(String firstname,String lastname){
